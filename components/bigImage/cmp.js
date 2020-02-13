6127fe3e71ecc01c465d819b65b765c78fa366d4
frenchleave <frenchleave1007@163.com>
2020-02-13 20:25:28 +0800

    project init

---
+++ components/bigImage/cmp.js
@@ -0,0 +1,35 @@
+// components/bigImage/cmp.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // imgSrc: {
+    //   type: String,
+    //   value: "",
+    //   observer(newValue, oldValue, changePath) {
+    //   }
+    // },
+    // mainTitle: {
+    //   type: String,
+    //   value: '',
+    //   observer() {}
+    // }
+    imgSrc: String,
+    mainTitle: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
